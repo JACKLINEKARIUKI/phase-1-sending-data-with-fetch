88f603d146939459e7284fda3ad2819c9586309a
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-29 00:02:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        const user = document.querySelector('body');
+        user.innerHTML = `<h1>${data.id}</h1>`;
+    })
+    .catch((errorObj) => {
+        const error = document.querySelector('body');
+        error.innerHTML = `<h1>${errorObj.message}</h1>`;
+      });
+}
